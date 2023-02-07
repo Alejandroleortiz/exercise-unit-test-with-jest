9b3e874a6078cf752631fc437152c82885a968ff
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-02-07 00:29:25 +0000

    finaliza

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ let fromDollarToYen = function(dollarValue){ //Dollar to Yen//
 	return dollarValue * 127.9;
 }
 
+let fromEuroToDollar = function(euroValue){
+  return euroValue * 1.2;
+}
+
 let fromYenToPound = function(yenValue){
     return yenValue * 0.0063;
   }
-module.exports = {fromDollarToYen, fromYenToPound};
+module.exports = {fromDollarToYen, fromYenToPound, fromEuroToDollar};
 
--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 
 // importar la función del archivo app.js
-const { fromDollarToYen,  fromYenToPound } = require('./app.js');
+const { fromDollarToYen,  fromYenToPound, fromEuroToDollar } = require('./app.js');
 // comienza tu primera prueba
 test('should return 127.9 if the parameter is one dollar', () => {
     //dentro de la prueba llamamos a nuestra función 
     let total1 = fromDollarToYen(1);
     // esperamos que la conversion sea
     expect(total1).toBe(127.9);
+});
+
+test('should return 1.2 if the parameter is one Euro', () => {
+    //dentro de la prueba llamamos a nuestra función 
+    let total3 = fromEuroToDollar(1);
+    // esperamos que la conversion sea
+    expect(total3).toBe(1.2);
 });
 
 test('should return 0.0063 if the parameter is one Yen', () => {
